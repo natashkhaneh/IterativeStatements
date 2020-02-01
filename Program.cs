8ef8c7c9fafeabc8e4bbbe343d57ef41ddaec9a6
Navid <Navid@DESKTOP-SKN5ILJ>
2020-01-31 23:33:51 -0500

    Added some comments and slight change in code.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace IterativeStatements
                 int input_value = int.Parse(input);
 
                 //Executes a For loop if the user input is between 0 and 100
-                if ((input_value >= 1) && (input_value <= 100))
+                if ((input_value > 0) && (input_value <= 100))
                 {
                     Console.WriteLine("Executing a For Loop!");
                     Console.WriteLine("The For Loop will iterate " + input_value.ToString() + " times.");
@@ -26,21 +26,26 @@ namespace IterativeStatements
                     // For loop
                     for (int i = 0; i < input_value; i++)
                     {
-                        Console.WriteLine("You have entered " + input_value.ToString() +". The current integer value in the loop is " + i.ToString() + ".");
+                        int j = i + 1;
+                        Console.WriteLine("You have entered " + input_value.ToString() +". The current integer value in the loop is " + j.ToString() + ".");
                     }
 
+                    //Prompts user to exit the program.
                     Console.WriteLine("Press any key to exit the program.");
                     Console.ReadKey(true);
 
                 }
             }
+            // End of try
 
             catch
             {
+                // Displays a message if user input is invalid.
                 Console.WriteLine("Please enter a valid integer value and running the program again.");
                 Console.WriteLine("Press any key to exit the program.");
                 Console.ReadKey(true);
             }
+            //End of catch
         }
     }
 }
